6855c6876592e7d2074cbca35c840b1eaadace71
c0d3m0nk3y <rickyoswald@gmail.com>
2014-06-03 07:57:40 +0100

    Closed sockets on exception

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,47 @@ import java.net.Socket;
 
 public class Main {
 	private static String inputLine;
+	private static ServerSocket serverSocket;
+	private static Socket clientSocket;
+	private static BufferedReader bufferedReader;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
-			ServerSocket serverSocket = new ServerSocket(63400);
-			Socket clientSocket = serverSocket.accept();
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			serverSocket = new ServerSocket(63455);
+			clientSocket = serverSocket.accept();
+			bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			while((inputLine = bufferedReader.readLine()) != null) {
 				System.out.println(inputLine);
 			}
 		} catch (Exception e) {
 			System.out.println(e);
+			closeSocket();
 		}
 		
+	}
+	
+	private static void closeSocket() {
+		try {
+			if(serverSocket != null) {
+				serverSocket.close();
+				serverSocket = null;
+			}
+			
+			if(clientSocket != null) {
+				clientSocket.close();
+				clientSocket = null;
+			}
+			
+			if(bufferedReader != null) {
+				bufferedReader.close();
+				bufferedReader = null;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
+	}
 
 }
